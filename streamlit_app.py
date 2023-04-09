4ec32ce43918a772d73b68619552475a5c47897c
Matt Wells <matt.n.wells@gmail.com>
2023-04-09 09:18:35 -0700

    Update streamlit_app.py
    Added breakfast section header and breakfast menu items.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
+<h1>Breakfast Menu</h1>
+
+<p>Omega-3 and Blueberry Oatmeal</p>
+
+<p>Kale, Spinach, and Rocket Smoothie</p>
+
+<p>Hard-boiled Free-Range Egg</p>
